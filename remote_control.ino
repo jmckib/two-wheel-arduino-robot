35aa1130447371d02ed93d1fc83cfac639af4a79
Jeremy McKibben-Sanders <jmckib2@gmail.com>
2012-07-24 23:36:43 -0700

    Allow remote control to be operated with nunchuk joystick.
    Doesn't handle variable speeds. It will only power the motors to full or not
    at all. I did this for now because analogWrite(val) for vals between 0 and
    255 doesn't seem to cause the motors to move, and I'm not sure why. Need to
    debug.

--- remote_control.ino
+++ remote_control.ino
@@ -1,25 +1,74 @@
 #include <assert.h>
+#include <Wire.h>
+#include <ArduinoNunchuk.h>
 #include <VirtualWire.h>
+
+ArduinoNunchuk nunchuk = ArduinoNunchuk();
 
 void setup() {
   Serial.begin(9600);
 
-  // Initialise the RF transmitter
+  // Initialize the RF transmitter.
   vw_set_ptt_inverted(true);  // Required for DR3100
   vw_setup(2000);  // Bits per sec
+
+  // Initialize the nunchuk.
+  nunchuk.setPowerPins();
+  nunchuk.init();
 }
 
+// The raw values when joystick untouched.
+int MED_JOY = 128;
+// When the absolute difference between MED_JOY and actual value
+// is larger than this, start moving.
+int JOY_THRESHOLD = 30;
+
 char msg[VW_MAX_MESSAGE_LEN];
-byte msg_len;
+
+int speed;
+char *direction;
 
 void loop() {
-  // Receive a newline-terminated message via USB and pass to robot.
-  if (msg_len = Serial.readBytesUntil('\n', msg, VW_MAX_MESSAGE_LEN)) {
-    // To make sure we don't get a partial messages, messages with
-    // length == VW_MAX_MESSAGE_LEN are not allowed.
-    assert(msg_len < VW_MAX_MESSAGE_LEN);
+  nunchuk.update();
 
-    vw_send((uint8_t *)msg, msg_len);
-    vw_wait_tx();  // Wait until the whole message is gone
+  int x_diff = nunchuk.joyX - MED_JOY;
+  int y_diff = nunchuk.joyY - MED_JOY;
+  int abs_diff;
+
+  if (abs(y_diff) > abs(x_diff)) {  // go forwards or backwards
+    abs_diff = abs(y_diff);
+
+    if (y_diff > 0) {
+      direction = "F";
+    } else {
+      direction = "B";
+    }
+  } else {  // go left or right
+    abs_diff = abs(x_diff);
+    if (x_diff > 0) {
+      direction = "R";
+    } else {
+      direction = "L";
+    }
   }
+
+  if (abs_diff > JOY_THRESHOLD) {
+    speed = 100;
+  } else {
+    speed = 0;
+  }
+
+  sprintf(msg, "%s,%d", direction, speed);
+
+  Serial.print("'");
+  Serial.print(msg);
+  Serial.println("'");
+
+  digitalWrite(13, HIGH);
+  vw_send((uint8_t *)msg, strlen(msg));
+  vw_wait_tx();  // Wait until the whole message is gone
+  digitalWrite(13, LOW);
+
+  // Sending commands more often than this won't make a difference.
+  delay(100);
 }
